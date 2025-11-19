2cbde89a8092a4bd4089ffb439a91c405de904a1
HectorMagdaleno <estoreals@gmail.com>
2025-11-19 10:41:26 -0600

    Feat: Se agregó que se puediera imprimir en el index.html

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,4 +1,20 @@
 
+//Seleccionamos el contenedor donde se mostrará el resultado que es del html "Div" con el id "fizzbuzz-container"
+const salida  = document.getElementById("fizzbuzz-container");
+
+//Función llamada "ImprimirFizBuzz" para imprimir el resultado en el contenedor seleccionado el cual le tendremos que pasar el mensaje a imprimir
+function ImprimirFizBuzz(Mensaje) {
+
+    //Creamos un párrafo para cada mensaje en la constante "parrafo" crea el parrafo "p"
+    const parrafo = document.createElement("p");
+    //Asignamos el mensaje a la variable parrafo
+    parrafo.textContent = Mensaje;
+    //Agregamos el párrafo al contenedor "Div" de la variable salida, con el appeldChild se agrega el parrafo dentro del contenedor "Div"
+    salida.appendChild(parrafo);
+}
+
+
+
 //Con el for inicializamos en 0 y terminamos en 100
 for (let i = 0; i < 101; i++) {
   
@@ -6,18 +22,24 @@ for (let i = 0; i < 101; i++) {
     
     //Primero verificamos si es divisible entre 3 y 5
  if(i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+
+    //Mandamos a llamar a la función ImprimirFizBuzz con el mensaje "FizzBuzz"
+    ImprimirFizBuzz("FizzBuzz");
  }
     //Luego verificamos si es divisible entre 3
     else if(i % 3 == 0) {
-    console.log("Fizz");
+        //Mandamos a llamar a la función ImprimirFizBuzz con el mensaje "Fizz"
+    ImprimirFizBuzz("Fizz");
     }
     //Luego verificamos si es divisible entre 5
     else if(i % 5 == 0) {
-    console.log("Buzz");
+        //Mandamos a llamar a la función ImprimirFizBuzz con el mensaje "Buzz"
+    ImprimirFizBuzz("Buzz");
     }
     //Si no es divisible entre ninguno, imprimimos el número
     else {
-    console.log(i);
+
+        //Mandamos a llamar a la función ImprimirFizBuzz con el número actual que esta dentro de la variable "i"
+    ImprimirFizBuzz(i);
     }
 }
